f87524f1fb802b678c0b32eccffaef5463fdd440
Nick Ferraro <the.nick.ferraro@gmail.com>
2020-11-26 12:22:16 -0800

    Save. Logging and connections

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 require('dotenv').config();
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const pino = require('pino');
+const Bot = require('./src/bot');
 const token = process.env.DISCORD_TOKEN;
+const log = pino();
 
-client.once('ready', () => {
-	console.log('Ready!');
-});
+let isShuttingDown = false;
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
 
-client.login(token);
+const bot = new Bot(token);
+
+startup()
+    .catch((e) => {
+        log.error('Catastrophic failure: ', e);
+        process.exit(1);
+    });
+
+async function startup() {
+    log.info('Starting up');
+    await bot.connect();
+}
+
+async function shutdown() {
+    if (isShuttingDown) return;
+
+    log.info('Shutting down connections');
+    isShuttingDown = true;
+
+    await bot.disconnect();
+    process.exit(0);
+}
--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,36 @@
+const Discord = require('discord.js');
+const pino = require('pino');
+const log = pino();
+
 class Bot {
-    constructor(key, options) {
-        
+    constructor(token, options) {
+        this.prefix = '!';
+        this.commands = [];
+        this.token = token;
+        this.client = new Discord.Client(options);
+
+        this.client.once('ready', (...args) => this.handleReady(...args));
+        this.client.on('message', (...args) => this.handleMessage(...args));
+    }
+
+    async connect() {
+        return this.client.login(this.token);
+    }
+
+    handleReady() {
+        log.info('Discord client ready');
+    }
+
+    handleMessage(message) {
+        log.info('Message received: %s', message.id);
+        if (message.content.startsWith(this.prefix)) {
+            log.info('Command received: %s', message.content);
+        }
+    }
+
+    async disconnect() {
+        return this.client.destroy();
     }
 }
+
+module.exports = Bot;
